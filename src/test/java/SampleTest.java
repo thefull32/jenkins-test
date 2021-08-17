86894f6894de347bd8d0daf708b104d8b4923e0e
Garrett Neuenkirchen <garrett.neuenkirchen@morningstar.com>
2021-08-17 12:11:57 -0500

    putting selenium into the mix

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SampleTest {
@@ -6,7 +8,12 @@ public class SampleTest {
     public void oneTest() {
         String baseUrl = "http://google.com";
 
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
         System.out.println("Here comes the testing!");
+
+        driver.quit();
     }
 
 }
