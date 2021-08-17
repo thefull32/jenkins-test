8791f8bc59c65e464d7dca715aa71a03f30b3b2c
Garrett Neuenkirchen <garrett.neuenkirchen@morningstar.com>
2021-08-17 12:34:53 -0500

    Adding another test class.

---
+++ src/test/java/AnotherTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+public class AnotherTest {
+
+    @Test
+    public void oneTest() {
+        String baseUrl = "http://google.com";
+
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        //System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
+        System.setProperty("webdriver.gecko.driver","src/main/resources/linux/geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+
+        System.out.println("Another Test?!?");
+
+        driver.quit();
+    }
+
+}
