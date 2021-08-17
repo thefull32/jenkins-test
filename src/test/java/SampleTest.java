3cb2285c7aab9e037749460940307322e3891b70
Garrett Neuenkirchen <garrett.neuenkirchen@morningstar.com>
2021-08-17 12:29:49 -0500

    time for headless.

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 public class SampleTest {
@@ -8,8 +10,13 @@ public class SampleTest {
     public void oneTest() {
         String baseUrl = "http://google.com";
 
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        //System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
         System.setProperty("webdriver.gecko.driver","src/main/resources/linux/geckodriver");
-        WebDriver driver = new FirefoxDriver();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
 
         System.out.println("Here comes the testing!");
 
